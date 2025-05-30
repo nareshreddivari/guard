73e50b6b9a08918b173e51dffea5b047e0bf0bd9
nareshreddivari <167742965+nareshreddivari@users.noreply.github.com>
2025-05-30 09:32:27 +0000

    Update index.ts

--- iac/index.ts
+++ iac/index.ts
@@ -1,7 +1,11 @@
+// import * as pulumi from "@pulumi/pulumi";
+// import * as azure from "@pulumi/azure-native";
+
+// // Sample ACA resource
+// const resourceGroup = new azure.resources.ResourceGroup("rg-demo");
+
+// export const resourceGroupName = resourceGroup.name;
 import * as pulumi from "@pulumi/pulumi";
-import * as azure from "@pulumi/azure-native";
 
-// Sample ACA resource
-const resourceGroup = new azure.resources.ResourceGroup("rg-demo");
-
-export const resourceGroupName = resourceGroup.name;
+const config = new pulumi.Config();
+export const message = "Pulumi setup complete!";
